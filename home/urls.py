2f1d6da97cb2b7a6ee475ae00c87d3f977abbf9e
hemant-prajapati <hp9685719326@gmail.com>
2023-01-15 11:25:45 +0530

    myproject commit

---
+++ home/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path
+from .import views
+urlpatterns = [
+
+    path("",views.index),
+    path("signin",views.signin),
+    path("signup",views.signup),
+    path("dashboard",views.dashboard),
+    path("viewstudents",views.viewstudents),
+    path("lgout",views.lgout),
+    path("course",views.course),
+    path("addcourses",views.addcourses),
+    path('deletecourse',views.deletecourse),
+    path('updatecourse',views.updatecourse),
+    path('searchcourse',views.searchcourse),
+    path('addstudent',views.addstudent),
+    path('deletestudent',views.deletestudent),
+    path('updatestudent',views.updatestudent),
+    path('searchstudent',views.searchstudent),
+    path('teacher',views.showTeacher),
+    path('addteacher',views.addteacher,name="addTeacher")
+]
+
---
+++ home/views.py
@@ -0,0 +1,192 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.hashers import check_password, make_password
+from django.contrib import messages
+from .models import *
+from django.contrib import auth
+from django.contrib.auth import authenticate,login,logout
+from django.db.models import Q
+
+def signup(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        pwd=make_password(request.POST['pwd'])
+        if User.objects.filter(Email=email).exists():
+            messages.info(request,'Email already exists ')
+            return render(request,'sign-up.html')    
+        else:
+            user=User.objects.create(Name=name,Email=email,Password=pwd)
+            user.save()
+            messages.success(request,'!!!!! Sucessfully Registered !!!!!')
+            return render(request,'index.html')
+    else:        
+        return render(request,"sign-up.html")
+
+def signin(request):
+    if request.method=='POST':
+        email=request.POST['email']
+        pwd_user=request.POST['pwd']
+        # user = auth.authenticate(Email=email,pwd=pwd_user)
+        if User.objects.filter(Email=email).exists():
+            obj=User.objects.get(Email=email)
+            pwd=obj.Password
+            if check_password(pwd_user,pwd):
+                return redirect('/dashboard')
+            else:
+                messages.error(request,' Password Incorrect')
+                return render (request,'index.html')                
+        else:
+            messages.error(request,' email is not registered')       
+            return render(request,"index.html") 
+    else:       
+        return render(request,"index.html")
+    
+        
+
+def index(request):
+    return render (request,'index.html')        
+
+def lgout(request):
+    auth.logout(request)
+    messages.info(request,'sucessfully logout ')
+    return render(request,'index.html') 
+
+def course(request):
+    data=Courses.objects.all()
+    return render (request,'courses.html',{'data':data})
+     
+
+def addcourses(request):
+    name=request.POST['name']
+    fees=request.POST['fees']
+    fees=int(fees)
+    duration=request.POST['duration']
+    textfield=request.POST['textfield']
+    if Courses.objects.filter(Course_name=name).exists():
+        messages.info(request,'Course already exists')
+        data=Courses.objects.all()
+        return render(request,'courses.html',{'data':data}) 
+    else:
+        Courses.objects.create(Course_name=name, Course_fees=fees,Course_textbox=textfield,Course_duration=duration)
+        messages.success(request,'!!!!Successfully Added!!!!')
+        data=Courses.objects.all()
+        return render(request,'courses.html',{'data':data}) 
+
+def deletecourse(request):
+    cid=request.GET['cid']
+    Courses.objects.get(id=cid).delete()
+    data=Courses.objects.all()
+    return render(request,"courses.html",{'data':data}) 
+
+def updatecourse(request):
+    uid = request.POST['id']
+    c = Courses.objects.filter(id=uid).get()
+    c.name=request.POST['name']
+    c.fees=request.POST['fees']
+    c.duration=request.POST['duration']
+    c.textfield=request.POST['textfield']   
+    c.save()
+    data=Courses.objects.all()
+    return render(request,'course.html',{'data':data})
+
+def searchcourse(request):
+    name=request.POST['name']
+    e=Courses.objects.get(name=name)
+    return render(request,'course.html',{'data':[e]})     
+
+def addstudent(request):
+    t=Student()
+    t.Student_Name=request.POST['name']
+    t.Student_email=request.POST['email']
+    t.Student_mobile_number=request.POST['contact']
+    t.Student_college=request.POST['college']
+    t.Student_degree=request.POST['degree']
+    tid=request.POST['course']
+    t.Student_due=request.POST['Due']
+    t.Student_paid=request.POST['Paid']
+    t.Student_total=request.POST['Total']
+    t.Course_name=Courses.objects.get(id=tid)
+    t.save()
+    st=Student.objects.all()
+    data=Courses.objects.all()
+    return render(request,'viewstudents.html',{'st':st,'data':data})
+
+def deletestudent(request):
+    id=request.GET['id']
+    Student.objects.filter(id=id).delete()
+    data=Courses.objects.all()
+    st=Student.objects.all()
+    return render(request,'viewstudents.html',{'data':data,'st':st}) 
+
+def updatestudent(request): 
+    t=Student()
+    t.id=request.POST['id']
+    t.name=request.POST['name']
+    t.email=request.POST['email']
+    t.contact=request.POST['contact']
+    t.college=request.POST['college']
+    t.degree=request.POST['degree']
+    tid=request.POST['course']
+    t.course=Courses.objects.get(id=tid)
+    t.save()
+    data=Courses.objects.all()
+    st=Student.objects.all()
+    return render(request,'viewstudents.html',{'data':data,'st':st})
+
+def searchstudent(request):
+    find=request.POST['name']
+    s=Student.objects.filter(Q(name=find) | Q(email=find) | Q(college=find)).all()
+    return render(request,'viewstudents.html',{'st':s})                                   
+
+def dashboard(request):
+    nm = User.objects.all()
+    data=Courses.objects.all()
+    count_course= Courses.objects.all().count()
+    count_student= Student.objects.all().count()
+    count_teacher = Teacher.objects.all().count()
+    return render (request,'dashboard.html',{'nm':nm,"count_teacher":count_teacher,'data':data,'count_course':count_course,'count_student':count_student})
+
+def viewstudents(request):
+    data=Courses.objects.all()
+    st=Student.objects.all()
+    return render(request,'viewstudents.html',{'data':data,'st':st})
+
+def showTeacher(request):
+    tc=Teacher.objects.all()
+    return render(request,'teacher.html',{"tc":tc})            
+
+def addteacher(request):
+        if request.method == "POST":
+            Teacher_name= request.POST["tname"]
+            Teacher_email= request.POST["temail"]
+            Teacher_mobile_number= request.POST["tmobile"]
+            Teacher_joining_date= request.POST["tjoindate"]
+            Teacher_education= request.POST["teducation"]
+            Teacher_employee_id= request.POST["tempid"]
+            Teacher_work_exp= request.POST["tworkexpe"]
+            teacher_pack=request.POST["tpack"]
+            if Teacher.objects.filter(Teacher_email=Teacher_email).exists():
+                messages.error(request, "Email id already exists")
+                return redirect('teacher')
+        
+            elif Teacher.objects.filter(Teacher_mobile_number=Teacher_mobile_number).exists():
+                messages.error(request, "Mobile Number already exists")
+                return redirect('teacher')
+            else:
+                Teacher.objects.create( Teacher_name=Teacher_name, 
+                                            Teacher_email=Teacher_email, 
+                                            Teacher_mobile_number=Teacher_mobile_number,
+                                            Teacher_joining_date=Teacher_joining_date,
+                                            Teacher_education=Teacher_education,
+                                            Teacher_employee_id=Teacher_employee_id,
+                                            Teacher_work_exp=Teacher_work_exp,
+                                            teacher_pack=teacher_pack
+                                            )
+                messages.success(request, "Teacher Added Successfully!!")
+                tc=Teacher.objects.all()
+                return render(request, 'teacher.html', {'tc':tc})
+                                                                
+        else:
+            tc=Teacher.objects.all()
+            return render(request, 'teacher.html', {'tc':tc})
+
